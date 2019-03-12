a7168cded7bcc00b70fa02a5ac4bc3aaa7b72cee
unknown <alvinsuanco@yahoo.com>
2019-03-12 12:59:57 +0800

    web portfolio update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,59 @@
-const cards = [
-  {title: 'Gooey PBJ Brownies', author: 'John Walibur', image: 'https://placeimg.com/640/480/nature'},
-  {title: 'Crisp Spanish Tortilla Matzo Brei', author: 'Colman Andrews', image: 'https://placeimg.com/640/480/animals'},
-  {title: 'Grilled Shrimp with Lemon and Garlic', author: 'Celeste Mills', image: 'https://placeimg.com/640/480/arch'}
-]
+const elems = document.querySelectorAll('.laya-please');
+const layer2 = document.querySelector('.layer-2');
+const layer3 = document.querySelector('.layer-3');
+const layer4 = document.querySelector('.layer-4');
+const layer5 = document.querySelector('.layer-5');
+const layer6 = document.querySelector('.layer-6');
+const layer7 = document.querySelector('.layer-7');
+const layer8 = document.querySelector('.layer-8');
 
-new Vue({
-  el: '#app',
-  data: {
-    cards: cards,
-    selectedCard: -1
-  },
-  methods: {
-    hoverCard(selectedIndex) {
-      this.selectedCard = selectedIndex
-      this.animateCards()
-    },
-    animateCards () {
-      this.cards.forEach((card, index) => {
-        const direction = this.calculateCardDirection(index, this.selectedCard)
-        TweenMax.to(
-          this.$refs[`card_${index}`], 
-          0.3, 
-          {x: direction * 50}
-        )
-      })
-    },
-    calculateCardDirection (cardIndex, selectedIndex) {
-      if(selectedIndex === -1) {
-        return 0
-      }
-      
-      const diff = cardIndex - selectedIndex
-      return diff === 0 ? 0 : diff/Math.abs(diff)
-    },
-    isSelected (cardIndex) {
-      return this.selectedCard === cardIndex
+
+setTimeout(function () {
+    elems.forEach(function (elem, index) {
+        elem.style.animation = "none";
+    });
+}, 1500);
+
+
+
+document.body.addEventListener('mousemove', function (e) {
+    if (!e.currentTarget.dataset.triggered) {
+        elems.forEach(function (elem, index) {
+            if (elem.getAttribute('style')) {
+                elem.style.transition = "all .5s";
+                elem.style.transform = "none";
+            }
+        });
     }
-  }
-})
+    e.currentTarget.dataset.triggered = true;
+    
+    let width = window.innerWidth / 2;
+    let mouseMoved2 = ((width - e.pageX) / 50);
+    let mouseMoved3 = ((width - e.pageX) / 40);
+    let mouseMoved4 = ((width - e.pageX) / 30);
+    let mouseMoved5 = ((width - e.pageX) / 20);
+    let mouseMoved6 = ((width - e.pageX) / 10);
+    let mouseMoved7 = ((width - e.pageX) / 5);
+
+    layer3.style.transform = "translateX(" + mouseMoved2 + "px)";
+    layer4.style.transform = "translateX(" + mouseMoved3 + "px)";
+    layer5.style.transform = "translateX(" + mouseMoved4 + "px)";
+    layer6.style.transform = "translateX(" + mouseMoved5 + "px)";
+    layer7.style.transform = "translateX(" + mouseMoved6 + "px)";
+    layer8.style.transform = "translateX(" + mouseMoved7 + "px)";
+});
+
+document.body.addEventListener('mouseleave', function (e) {
+    elems.forEach(function (elem, index) {
+        elem.style.transition = "all .5s";
+        elem.style.transform = "none";
+    });
+});
+
+document.body.addEventListener('mouseenter', function (e) {
+    elems.forEach(function (elem, index) {
+        setTimeout(function () {
+            elem.style.transition = "none";
+        }, 500);
+    });
+});S
